8314d699cf99907569ce22dfa90dcd396c415b8e
ph-moura <phmlinux@gmail.com>
2022-01-11 22:37:45 -0300

    Setting up my scripts.

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/    /*Command*/     /*Update Interval*/    /*Update Signal*/
+    {"  "  , "disk /home"          ,   60  ,    5}, 
+    {"  "  , "dwm_mem "            ,   1   ,    6},
+    {"  "  , "ipca"                ,   3600,    7},
+    {"  "  , "euro"                ,   3600,    8},
+    {"  "  , "dolar"               ,   3600,    9},
+    {""     , "volume"              ,   0   ,   10},
+    {"  "  , "date '+%a, %d de %b'",   1   ,   11},
+    {"  "  , "date '+%H:%M:%S'"    ,   1   ,   12},
+    {" "    , "ac_bat"              ,   5   ,   13},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ";
+static unsigned int delimLen = 1;
